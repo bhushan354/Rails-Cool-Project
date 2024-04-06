dcc63378f616963553c32f45d64fb3e66dd975a1
bhushan354 <deshmukhumesh354@gmail.com>
2024-04-06 08:47:02 +0530

    Add all required routes and basic setup for components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+} from "react-router-dom";
 import { Layout, Typography, Space } from 'antd';
 //these components we can import in single line which are exported from './components/index.js' file
-import { Navbar } from './components';
+import { Exchanges, Homepage, News, Cryptocurrencies, CryptoDetails, Navbar } from './components';
 
 const App = () => {
     return (
         <div className='app'>
             <div className='navbar'>
-              <Navbar />
+                <Navbar />
             </div>
             <div className='main'>
-
+                <Layout>
+                    <div className='routes'>
+                        <Routes>
+                            <Route exact path="/" element={<Homepage />} />
+                            <Route exact path="/exchanges" element={<Exchanges />} />
+                            <Route exact path="/cryptocurrencies" element={<Cryptocurrencies />} />
+                            <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
+                            <Route exact path="/news" element={<News />} />
+                        </Routes>
+                    </div>
+                </Layout>
             </div>
             <div className='footer'>
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     return (
         <div className='nav-container'>
             <div className='logo-container'>
-                <Avatar src={icon} size="large"/>
+                <Avatar src={icon} size="large" />
                 <Typography.Title level={2} className='logo'>
                     <Link to="/">Cryptoverse</Link>
                 </Typography.Title>
@@ -17,6 +17,20 @@ const Navbar = () => {
 
                 </Button> */}
             </div>
+            <Menu theme='dark'>
+                <Menu.Item icon={<HomeOutlined />}>
+                    <Link to="/">Home</Link>
+                </Menu.Item>
+                <Menu.Item icon={<FundOutlined />}>
+                    <Link to="/Cryptocurrencies">cryptocurrencies</Link>
+                </Menu.Item>
+                <Menu.Item icon={<MoneyCollectOutlined />}>
+                    <Link to="/exchanges">Exchanges</Link>
+                </Menu.Item>
+                <Menu.Item icon={<HomeOutlined />}>
+                    <Link to="/news">News</Link>
+                </Menu.Item>
+            </Menu>
 
         </div>
     )
