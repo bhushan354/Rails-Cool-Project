b19f435fea073f241cb7b4968232a1058083622d
bhushan354 <deshmukhumesh354@gmail.com>
2024-04-09 09:49:15 +0530

    routes setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Link,
-} from "react-router-dom";
+import { Routes, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 //these components we can import in single line which are exported from './components/index.js' file
 import { Exchanges, Homepage, News, Cryptocurrencies, CryptoDetails, Navbar } from './components';
@@ -17,19 +12,22 @@ const App = () => {
             </div>
             <div className='main'>
                 <Layout>
-                    <div className='routes'>
+                    <div className="routes">
                         <Routes>
-                            <Route exact path="/" element={<Homepage />} />
-                            <Route exact path="/exchanges" element={<Exchanges />} />
-                            <Route exact path="/cryptocurrencies" element={<Cryptocurrencies />} />
-                            <Route exact path="/crypto/:coinId" element={<CryptoDetails />} />
-                            <Route exact path="/news" element={<News />} />
+                            <Route path="/" element={<Homepage />} />
+                            <Route path="/exchanges" element={<Exchanges />} />
+                            <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+                            <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+                            <Route path="/news" element={<News />} />
                         </Routes>
                     </div>
                 </Layout>
             </div>
             <div className='footer'>
-
+                <Typography.Title>
+                    Cryptoverser<br />
+                    All rights reserved
+                </Typography.Title>
             </div>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import 'antd/dist/reset.css';
+
 
 ReactDOM.render(
   <Router>
